82d295a1bff5946d126bf1eb44b98bf66b7e8e15
victorjanees <victorjanees@gmail.com>
2022-06-25 14:38:29 +0530

    Scenario-1 Completed

--- src/com/victor/olympics/Main.java
+++ src/com/victor/olympics/Main.java
@@ -4,18 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        List<Event> events = getAthleteEventsData();
+        List<Region> regions = getNocRegionsData();
+        findYearWiseNumberOfGoldMedalsWonByEachPlayers(events);
+//        findAthletesWhoWonGoldMedalIn2000AndAgeIsLessThan30Years(events);
 
     }
 
-    public static List<AthleteEvent> getAthleteEventsData() {
-        String path = "src/com/victor/olympics/athlete_events.csv";
+    public static List<Event> getAthleteEventsData() {
+        String path = "resources/Athlete.csv";
         String line = "";
         int header = 1;
-        List<AthleteEvent> athleteEvents = new ArrayList<>();
+        List<Event> events = new ArrayList<>();
         try {
             BufferedReader dataReader = new BufferedReader(new FileReader(path));
             while ((line = dataReader.readLine()) != null) {
@@ -24,36 +30,90 @@ public class Main {
                     continue;
                 }
                 String[] athleteEventData = line.split(",");
-                AthleteEvent athleteEvent = new AthleteEvent();
+                Event event = new Event();
 
-                Integer id = Integer.parseInt(athleteEventData[0]);
                 Integer age = Integer.parseInt(athleteEventData[3]);
-                Integer height = Integer.parseInt(athleteEventData[4]);
-                Integer weight = Integer.parseInt(athleteEventData[5]);
                 Integer year = Integer.parseInt(athleteEventData[9]);
 
-                athleteEvent.setId(id);
-                athleteEvent.setName(athleteEventData[1]);
-                athleteEvent.setSex(athleteEventData[2]);
-                athleteEvent.setAge(age);
-                athleteEvent.setHeight(height);
-                athleteEvent.setWeight(weight);
-                athleteEvent.setTeam(athleteEventData[6]);
-                athleteEvent.setNoc(athleteEventData[7]);
-                athleteEvent.setGames(athleteEventData[8]);
-                athleteEvent.setYear(year);
-                athleteEvent.setSeason(athleteEventData[10]);
-                athleteEvent.setCity(athleteEventData[11]);
-                athleteEvent.setSport(athleteEventData[12]);
-                athleteEvent.setEvent(athleteEventData[13]);
-                athleteEvent.setMedal(athleteEventData[14]);
-                athleteEvents.add(athleteEvent);
+                event.setId(athleteEventData[0]);
+                event.setName(athleteEventData[1]);
+                event.setSex(athleteEventData[2]);
+                event.setAge(age);
+                event.setHeight(athleteEventData[4]);
+                event.setWeight(athleteEventData[5]);
+                event.setTeam(athleteEventData[6]);
+                event.setNoc(athleteEventData[7]);
+                event.setGames(athleteEventData[8]);
+                event.setYear(year);
+                event.setSeason(athleteEventData[10]);
+                event.setCity(athleteEventData[11]);
+                event.setSport(athleteEventData[12]);
+                event.setEvent(athleteEventData[13]);
+                event.setMedal(athleteEventData[14]);
+                events.add(event);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (Exception e) {
+            System.out.println("Exception Handled");;
+        }
+        return events;
+    }
+
+    public static List<Region> getNocRegionsData() {
+        String path = "resources/noc_regions.csv";
+        String line = "";
+        int header = 1;
+        List<Region> regions = new ArrayList<>();
+        try {
+            BufferedReader dataReader = new BufferedReader(new FileReader(path));
+            while ((line = dataReader.readLine()) != null) {
+                if (header == 1) {
+                    header = header + 1;
+                    continue;
+                }
+                String[] nocRegionsData = line.split(",");
+                Region region = new Region();
+
+                region.setNoc(nocRegionsData[0]);
+                region.setRegion(nocRegionsData[1]);
+                regions.add(region);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         } catch (Exception e) {
             e.printStackTrace();
         }
-return athleteEvents;
+        return regions;
     }
+
+    public static void findYearWiseNumberOfGoldMedalsWonByEachPlayers(List<Event> events) {
+        HashMap<Integer, HashMap> yearWiseNumberOfGoldMedalsWonByEachPlayer = new HashMap<>();
+
+        for (Event player :events){
+            String medal = player.getMedal();
+            if (medal.contains("Gold")){
+                Integer year = player.getYear();
+                String name = player.getName();
+                if (yearWiseNumberOfGoldMedalsWonByEachPlayer.containsKey(year)) {
+                    HashMap peopleWonGold = yearWiseNumberOfGoldMedalsWonByEachPlayer.get(year);
+                    if (peopleWonGold.containsKey(name)){
+                        int medalsCount = (int) peopleWonGold.get(name);
+                        peopleWonGold.put(name,medalsCount+=1);
+                    }
+                    else{
+                        peopleWonGold.put(name,1);
+                    }
+                } else {
+                    HashMap<String, Integer> numberOfGoldMedalsWonByEachPlayer = new HashMap<>();
+                    numberOfGoldMedalsWonByEachPlayer.put(name,1);
+                    yearWiseNumberOfGoldMedalsWonByEachPlayer.put(year,numberOfGoldMedalsWonByEachPlayer );
+                }
+            }
+        }
+        System.out.println(yearWiseNumberOfGoldMedalsWonByEachPlayer);
+
+
+    }
+
 }
